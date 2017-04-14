64d0d5a2daa8ef228224543ad024a2e7483ad9fc
Samuel Pavon <festovalros@gmail.com>
2017-04-14 06:30:43 -0400

    mejora el script

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
+
 # odoo_devdeploy
-bash script for odoo development deploy
+script de bash para despliege de instancias de desarrollo odoo con docker
+
+##Requerimientos
++ Docker
+
+##Uso
++ Correr el archivo deploy.sh en consola y seguir las instrucciones.
++ Abrir la instancia de odoo mediante la url http://localhost:<puerto de escucha>
+___
+
+# odoo_devdeploy
+bash script for odoo development deploy with docker
+
+##Requirements
++ Docker
+
+##Use
++ Run the file deploy.sh in console and follow the instructions.
++ Open the odoo instance in http://localhost:<listening port>
--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 # This script will test if you have given a leap year or not.
+echo 'indica el numero de puerto de escucha para la instancia odoo, seguido de [ENTER]:'
+
+read port
 
 echo 'escribe la version de odoo que quieres desplegar, seguido de [ENTER] (valores permitidos:"8", "9", "10":'
 
 read version
 
-if [ $version == 8 ]; then
-	sudo mkdir -p /opt/odoo8/addons /opt/odoo8/config /opt/odoo8/log
-	sudo chmod -R 755 /opt/odoo8/
-	sudo chmod -R 777 /opt/odoo8/log
-	sudo cp ./openerp-server.conf /opt/odoo8/config
-	docker run -d -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo --name db8 postgres:9.4
-	docker run -d -v /opt/odoo8/addons:/mnt/extra-addons -v /opt/odoo8/log:/var/log/odoo -v /opt/odoo8/config:/etc/odoo -p 8069:8069 --name odoo8 --link db8:db -t odoo:8
-elif [ $version == 9 ]; then
-	sudo mkdir -p /opt/odoo9/addons /opt/odoo9/config /opt/odoo9/log
-	sudo chmod -R 755 /opt/odoo9/
-	sudo chmod -R 777 /opt/odoo9/log
-	sudo cp ./openerp-server.conf /opt/odoo9/config
-	docker run -d -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo --name db9 postgres:9.4
-	docker run -d -v /opt/odoo9/addons:/mnt/extra-addons -v /opt/odoo9/log:/var/log/odoo -v /opt/odoo9/config:/etc/odoo -p 8070:8069 --name odoo9 --link db9:db -t odoo:9
+if [ $version = 8 ] || [ $version = 9 ] || [ $version = 10]; then
+	sudo mkdir -p /opt/odoo$version/addons /opt/odoo$version/config /opt/odoo$version/log
+	sudo chmod -R 755 /opt/odoo$version/
+	sudo chmod -R 777 /opt/odoo$version/log
+	if [ $version = 8 ] || [ $version = 9 ]; then
+		sudo cp ./openerp-server.conf /opt/odoo$version/config
+	elif [ $version = 10 ]; then
+		sudo cp ./odoo.conf /opt/odoo$version/config
+	fi
+	docker run -d -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo --name db$version postgres:9.4
+	docker run -d -v /opt/odoo$verion/addons:/mnt/extra-addons -v /opt/odoo$version/log:/var/log/odoo -v /opt/odoo$version/config:/etc/odoo -p $port:8069 --name odoo$version --link db$version:db -t odoo:$version
 else
-	sudo mkdir -p /opt/odoo10/addons /opt/odoo10/config /opt/odoo10/log
-	sudo chmod -R 755 /opt/odoo10
-	sudo chmod -R 777 /opt/odoo10/log
-	sudo cp ./odoo.conf /opt/odoo10/config
-	docker run -d -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo --name db9 postgres:9.4
-	docker run -d -v /opt/odoo10/addons:/mnt/extra-addons -v /opt/odoo10/log:/var/log/odoo -v /opt/odoo10/config:/etc/odoo -p 8071:8069 --name odoo10 --link db9:db -t odoo:10
+	echo 'no existe la versión'
 fi
 

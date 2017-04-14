e49b7eff3e525adf1c1cfe214aa0bd4e1e793ab9
Samuel Pavon <festovalros@gmail.com>
2017-04-14 07:06:22 -0400

    corregido deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,17 @@ echo 'escribe la version de odoo que quieres desplegar, seguido de [ENTER] (valo
 
 read version
 
-if [ $version = 8 ] || [ $version = 9 ] || [ $version = 10]; then
+if [ $version = 8 ] || [ $version = 9 ] || [ $version = 10 ]; then
 	sudo mkdir -p /opt/odoo$version/addons /opt/odoo$version/config /opt/odoo$version/log
 	sudo chmod -R 755 /opt/odoo$version/
 	sudo chmod -R 777 /opt/odoo$version/log
 	if [ $version = 8 ] || [ $version = 9 ]; then
 		sudo cp ./openerp-server.conf /opt/odoo$version/config
-	elif [ $version = 10 ]; then
+	else
 		sudo cp ./odoo.conf /opt/odoo$version/config
 	fi
 	docker run -d -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo --name db$version postgres:9.4
-	docker run -d -v /opt/odoo$verion/addons:/mnt/extra-addons -v /opt/odoo$version/log:/var/log/odoo -v /opt/odoo$version/config:/etc/odoo -p $port:8069 --name odoo$version --link db$version:db -t odoo:$version
+	docker run -d -v /opt/odoo$version/addons:/mnt/extra-addons -v /opt/odoo$version/log:/var/log/odoo -v /opt/odoo$version/config:/etc/odoo -p $port:8069 --name odoo$version --link db$version:db -t odoo:$version
 else
 	echo 'no existe la versión'
 fi

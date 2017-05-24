d32b2c163019a6f6352a8de20c7322e62e2a6986
Samuel Pavon <festovalros@gmail.com>
2017-05-24 02:10:26 -0400

    [FIX] corrige error de tipeo

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ PKG_OK=$(dpkg-query -W --showformat='${Status}\n' docker-ce|grep "install ok ins
 echo "Revisando instalacion de docker: $PKG_OK"
 if [ "" == "$PKG_OK" ]; then
   echo "No instalado. empezando instalacion."
-  sudo apt-get remove docker docker-engine; sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+  sudo apt-get remove docker docker-engine 
+  sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
   curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
   sudo apt-get update
@@ -36,7 +37,7 @@ if [ $version = 8 ] || [ $version = 9 ] || [ $version = 10 ]; then
 		sudo cp ./openerp-server.conf /opt/$name/config
 	else
 		sudo cp ./odoo.conf /opt/$name/config
-	fi
+	fi 
 	docker run -d -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo --name db$name postgres:9.4
 	docker run -d -v /opt/$name/addons:/mnt/extra-addons -v /opt/$name/log:/var/log/odoo -v /opt/$name/config:/etc/odoo -p $port:8069 --name $name --link db$name:db -t odoo:$version
 else

05da809c6896f08a96dfc24a1d7d09f4711160dc
Samuel Pavon <festovalros@gmail.com>
2019-11-06 14:37:39 -0500

    [ADD] agrega version 13 de odoo

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ echo 'indica el numero de puerto de escucha para la instancia odoo, seguido de [
 
 read port
 
-echo 'escribe la version de odoo que quieres desplegar, seguido de [ENTER] (valores permitidos:"11", "12":'
+echo 'escribe la version de odoo que quieres desplegar, seguido de [ENTER] (valores permitidos:"11", "12","13":'
 
 read version
 
@@ -40,14 +40,14 @@ else
   echo "auto-inicio desactivado"
 fi
 
-if [ $version = 11 ] || [ $version = 12 ]; then
+if [ $version = 11 ] || [ $version = 12 ] |[ $version = 13 ]; then
 	sudo mkdir -p /opt/$name/addons /opt/$name/config /opt/$name/log
 	sudo chmod -R 755 /opt/$name/
 	sudo chmod -R 777 /opt/$name/log
 	sudo cp ./odoo.conf /opt/$name/config
 else
-	echo "version no valida"	
-fi 
+	echo "version no valid"
+fi
 	sudo docker run -d -e POSTGRES_USER=odoo -e POSTGRES_DB=postgres -e POSTGRES_PASSWORD=odoo$restart --name db$name postgres:10
 	sudo docker run -d -v /opt/$name/addons:/mnt/extra-addons -v /opt/$name/log:/var/log/odoo -v /opt/$name/config:/etc/odoo -p $port:8069$restart --name $name --link db$name:db -t odoo:$version
 
